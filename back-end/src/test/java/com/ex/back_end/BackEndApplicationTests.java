91453673fa68581e72a5e66732cb09c0b6e58dd4
euler <eullerborgesdamotta155@gmail.com>
2025-11-16 23:58:18 -0300

    first commit

---
+++ back-end/src/test/java/com/ex/back_end/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
