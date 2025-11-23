5b05e21d5c2bf769a9b1f32a6951eedcdbe46f26
euler <eullerborgesdamotta155@gmail.com>
2025-11-23 00:57:20 -0300

    back

--- back-end/src/main/java/com/ex/back_end/controller/AuthController.java
+++ back-end/src/main/java/com/ex/back_end/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.ex.back_end.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(path = "/auth")
 public class AuthController {
 
+    @GetMapping
+    public void login(){
 
+    }
 }
---
+++ back-end/src/main/java/com/ex/back_end/repository/userRepository.java
@@ -0,0 +1,7 @@
+package com.ex.back_end.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class userRepository {
+}
